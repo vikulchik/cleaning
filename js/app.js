866309df504f1885ec80f909f6cfb79a4ff64d84
Vika Kovalenko <kvvendetta@gmail.com>
2017-06-27 13:14:59 +0300

    Add fonts.

--- js/app.js
+++ js/app.js
@@ -15,8 +15,24 @@ $(document).ready(function () {
     })
 });
 
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-
+;(function(){
+    var view = $('.clean-list__view');
+    view.on('click', '.clean-list__link', function(e){
+        e.preventDefault();
+        var item = $(this).closest(view),
+            content = $('.clean-list__section'),
+            items = $(this).next();
+        if(!content.hasClass('active-section')) {
+            $(this).removeClass('active-section');
+            items.addClass('active-section');
+        }else{
+            content.removeClass('active-section');
+        }
+        if(content.hasClass('active-section')){
+            $(this).addClass('active-link');
+            $('.clean-list__link').next().removeClass('active-link');
+        }else {
+            $('.clean-list__link').removeClass('active-link');
+        }
     });
-});
+}());
